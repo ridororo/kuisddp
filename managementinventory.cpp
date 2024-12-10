590fa0b3534e8cf069a712dd14549a3187c78e06
talithasalsabila19 <jiminsayang1995@gmail.com>
2024-12-10 22:46:31 +0700

    Update managementinventory.cpp

--- managementinventory.cpp
+++ managementinventory.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 
 int main(){
+  
+while (1){
+	
+		system("cls");
+		cout<<"----------------------"<<endl;
+		cout<<"      INVENTORY       "<<endl;
+		cout<<"----------------------"<<endl;
+		cout<<"1. Tambah Barang"<<endl;
+		cout<<"2. Tampilkan List Barang"<<endl;
+		cout<<"3. Hapus Barang"<<endl;
+		cout<<"4. Keluar"<<endl;
+		cout<<"silahkan pilih opsi [1/4]: ";
 
+		
+		cin>>pilih;
+	}
+  
   return 0;
 }
