fc7faef01e9428f83362c208c69a782983ac3185
rforridho <filuthrido237@gmail.com>
2024-12-10 22:45:06 +0700

    menambahkan library

--- managementinventory.cpp
+++ managementinventory.cpp
@@ -1,15 +1,4 @@
-while (1){
-	
-		system("cls");
-		cout<<"----------------------"<<endl;
-		cout<<"      INVENTORY       "<<endl;
-		cout<<"----------------------"<<endl;
-		cout<<"1. Tambah Barang"<<endl;
-		cout<<"2. Tampilkan List Barang"<<endl;
-		cout<<"3. Hapus Barang"<<endl;
-		cout<<"4. Keluar"<<endl;
-		cout<<"silahkan pilih opsi [1/4]: ";
-
-		
-		cin>>pilih;
-	}
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
